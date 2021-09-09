9327f8559f981f09be44fe2a49b5d2a71ed3ce96
Deepak Sharma <ds262969@gmail.com>
2021-09-09 22:57:48 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/home.dart';
+import 'package:flutter_practice/pages/home.dart';
+import 'package:flutter_practice/pages/login.dart';
 
 void main() {
   runApp(PracticeApp());
@@ -8,6 +9,20 @@ void main() {
 class PracticeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Home());
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => Login(),
+        "/login": (context) => Login(),
+        "/home": (context) => Home(),
+      },
+    );
   }
 }
